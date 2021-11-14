a1b497f52a9fd370ea4bceba5a98645d0523dfa8
Maruf <maruf.alom@portonics.com>
2021-11-14 16:00:51 +0600

    added jest

---
+++ longestSubstringWithoutRepeat/index.js
@@ -0,0 +1,13 @@
+function lengthOfLongestSubstr(s) {
+    let windowCharMap = {};
+    let windowStart = 0;
+    let maxLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const element = s[i];
+        
+    }   
+
+}
+
+module.exports = lengthOfLongestSubstr;
