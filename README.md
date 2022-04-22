9dd683da16fd9ee37b0623e483e635ece8fce8a3
Maruf <maruf.alom@portonics.com>
2022-04-22 22:49:34 +0600

    typo and alternate contains duplicate solution

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## Leetcode Solutions
 
 Just another random repo for keeping track of my leetcode progress.
-Note: I may not always provide most `optical` solutions to every problems
+Note: I may not always provide most `optimal` solutions to every problems
 Sometimes I can be messed up. Keep that in mind 
 :peace: 
 
--- contains-duplicate/contains-duplicate.js
+++ contains-duplicate/contains-duplicate.js
@@ -1,4 +1,10 @@
 // https://leetcode.com/problems/contains-duplicate/
+/************ Notes/ Edge cases
+ *  1. What if empty array
+ *  2. Negetive number? 
+ *  3. What if array contains single element?
+ * 
+ */
 
 
 // Brute force
@@ -6,18 +12,58 @@
  * @param {number[]} nums
  * @return {boolean}
  */
- const containsDuplicate = (nums) => {
-    let currentItem = [];
+//  const containsDuplicate = (nums) => {
+//     let currentItem = [];
+//     for (const num of nums) {
+//         if(currentItem.includes(num)) {
+//             return true;
+//         }
+//         currentItem.push(num);
+//     }
+
+//     return false;
+// };
+
+
+
+
+
+/**
+ * HasMap solution
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ function containsDuplicate(nums) {
+    const dupArr = {};
     for (const num of nums) {
-        if(currentItem.includes(num)) {
+        if(dupArr[num] === undefined) {
+            dupArr[num] = num
+        } else {
             return true;
         }
-        currentItem.push(num);
     }
 
     return false;
 };
 
+/**
+ * Set Solution
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ function containsDuplicate (nums) {
+    const dupArr = {};
+    for (const num of nums) {
+        if(dupArr[num] === undefined) {
+            dupArr[num] = num
+        } else {
+            return true;
+        }
+    }
 
+    return false;
+};
+
+//containsDuplicate([1,2,3,1])
 console.log(containsDuplicate([1,2,3,1]), true);
 console.log(containsDuplicate([1,2,3,4]), false);
