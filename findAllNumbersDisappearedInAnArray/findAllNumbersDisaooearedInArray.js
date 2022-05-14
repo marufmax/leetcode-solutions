838d7a5d28c75e9d189f568314f7224bab532fb6
Maruf <maruf.alom@portonics.com>
2022-05-14 22:36:03 +0600

    sol 3: are occurances are equal

--- findAllNumbersDisappearedInAnArray/findAllNumbersDisaooearedInArray.js
+++ findAllNumbersDisappearedInAnArray/findAllNumbersDisaooearedInArray.js
@@ -1,2 +1,19 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
 
+/**
+ * This is slowest solution, it gets time limit exceed
+ * 
+ * Time: O(n^2)
+ * Space: O(n)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ function findDisappearedNumbers(nums) {
+     const maxLength = nums.length
+     const allNumbers = Array.from({length: maxLength}, (_, i) => i + 1)
+
+     return allNumbers.filter(value => !nums.includes(value))
+
+}
+
+console.log(findDisappearedNumbers([8,3,2,7,4,2,3,1]))
